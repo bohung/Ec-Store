2ee039ad805a1e2611c344816608c7348dbad67f
bohung <monkeytai3@gmail.com>
2016-11-03 22:34:13 +0800

    config/environments/production.rb 上的 pay2go 設定成開發模式

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.after_initialize do
-    Pay2go.integration_mode = :production
+    Pay2go.integration_mode = :development
   end
 
   config.action_mailer.default_url_options = { host: 'https://git.heroku.com/hidden-eyrie-70814.git' } # 你的 heroku app 網址
