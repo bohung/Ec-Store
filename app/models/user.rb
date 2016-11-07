594572d3988a764245f44688c21ad596a600b525
bohung <monkeytai3@gmail.com>
2016-11-07 16:45:49 +0800

    Controller 新增建立 Facebook 登入的使用者資料 與 Facebook 登入加入選單

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,11 @@ class User < ActiveRecord::Base
   def to_normal
     self.update_columns(is_admin: false)
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
